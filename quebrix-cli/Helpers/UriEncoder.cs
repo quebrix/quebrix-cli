de9199f8ee0531f4e30de6904395c7cc08296387
safi.saeed1999 <safi.saeed1999@gmail.com>
2024-09-05 12:57:30 +0330

    add url encoder

---
+++ quebrix-cli/Helpers/UriEncoder.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using System.Web;
+
+namespace quebrix.Helpers;
+
+public static class UriEncoder
+{
+    public static string EncodeUrl(this string text) => HttpUtility.UrlEncode(text);
+
+    public static string DecodeUri(this string text) => HttpUtility.UrlDecode(text);
+}
+
